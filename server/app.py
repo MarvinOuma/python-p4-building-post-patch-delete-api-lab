056dbd8c4837c6b7efc5cd3fb8fa606efcf9d71d
MarvinDanielOduor <marvin.ouma@strathmore.edu>
2025-06-24 19:49:08 +0300

    Lab Complete

--- server/app.py
+++ server/app.py
@@ -45,5 +45,47 @@ def most_expensive_baked_good():
     most_expensive_serialized = most_expensive.to_dict()
     return make_response( most_expensive_serialized,   200  )
 
+@app.route('/baked_goods', methods=['POST'])
+def create_baked_good():
+    new_baked_good = BakedGood(
+        name=request.form.get("name"),
+        price=request.form.get("price"),
+        bakery_id=request.form.get("bakery_id")
+    )
+    
+    db.session.add(new_baked_good)
+    db.session.commit()
+    
+    baked_good_dict = new_baked_good.to_dict()
+    
+    return make_response(baked_good_dict, 201)
+
+@app.route('/bakeries/<int:id>', methods=['PATCH'])
+def update_bakery(id):
+    bakery = Bakery.query.filter_by(id=id).first()
+    
+    if request.form.get("name"):
+        bakery.name = request.form.get("name")
+    
+    db.session.add(bakery)
+    db.session.commit()
+    
+    bakery_dict = bakery.to_dict()
+    
+    return make_response(bakery_dict, 200)
+
+@app.route('/baked_goods/<int:id>', methods=['DELETE'])
+def delete_baked_good(id):
+    baked_good = BakedGood.query.filter_by(id=id).first()
+    
+    db.session.delete(baked_good)
+    db.session.commit()
+    
+    response_body = {
+        "message": "record successfully deleted"
+    }
+    
+    return make_response(response_body, 200)
+
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
